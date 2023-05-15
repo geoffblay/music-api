6bab364c44cd9c50a6677a28bab7461247da4d75
crobin27 <crobin27@calpoly.edu>
2023-05-15 11:05:51 -0700

    removing cruft

--- src/database.py
+++ src/database.py
@@ -21,24 +21,9 @@ def database_connection_url():
     return f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
 
 
-# DO NOT CHANGE THIS TO BE HARDCODED. ONLY PULL FROM ENVIRONMENT VARIABLES.
-dotenv.load_dotenv()
-supabase_api_key = os.environ.get("SUPABASE_API_KEY")
-supabase_url = os.environ.get("SUPABASE_URL")
-
 # create the database engine
 engine = sqlalchemy.create_engine(database_connection_url())
 
-
-if supabase_api_key is None or supabase_url is None:
-    raise Exception(
-        "You must set the SUPABASE_API_KEY and SUPABASE_URL environment variables."
-    )
-
-
-supabase: Client = create_client(supabase_url, supabase_api_key)
-
-sess = supabase.auth.get_session()
 
 metadata_obj = sqlalchemy.MetaData()
 # *********************************************************************************
@@ -52,7 +37,6 @@ def try_parse(type, val):
         return None
 
 
-test = sqlalchemy.Table("test", metadata_obj, autoload_with=engine)
 tracks = sqlalchemy.Table("tracks", metadata_obj, autoload_with=engine)
 playlists = sqlalchemy.Table("playlists", metadata_obj, autoload_with=engine)
 albums = sqlalchemy.Table("albums", metadata_obj, autoload_with=engine)
