22edd77c6b4977df976c981d2f4652b0bd8de020
crobin27 <crobin27@calpoly.edu>
2023-05-15 11:15:22 -0700

    removed supabase connections, updated personal gitignore

--- src/database.py
+++ src/database.py
@@ -7,10 +7,6 @@ import sqlalchemy
 from datetime import datetime
 
 
-# Supabase and Engine setup
-# *********************************************************************************
-
-
 def database_connection_url():
     dotenv.load_dotenv()
     DB_USER: str = os.environ.get("POSTGRES_USER")
@@ -23,25 +19,11 @@ def database_connection_url():
 
 # DO NOT CHANGE THIS TO BE HARDCODED. ONLY PULL FROM ENVIRONMENT VARIABLES.
 dotenv.load_dotenv()
-supabase_api_key = os.environ.get("SUPABASE_API_KEY")
-supabase_url = os.environ.get("SUPABASE_URL")
 
 # create the database engine
 engine = sqlalchemy.create_engine(database_connection_url())
-
-
-if supabase_api_key is None or supabase_url is None:
-    raise Exception(
-        "You must set the SUPABASE_API_KEY and SUPABASE_URL environment variables."
-    )
-
-
-supabase: Client = create_client(supabase_url, supabase_api_key)
-
-sess = supabase.auth.get_session()
-
 metadata_obj = sqlalchemy.MetaData()
-# *********************************************************************************
+
 # *********************************************************************************
 
 
