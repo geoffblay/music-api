d1acb67519048254cc307c342069c8eb441c1836
Geoff Blaylock <gblaylock2024@gmail.com>
2023-05-20 16:05:35 -0700

    boilerplate v2

---
+++ src/weather.py
@@ -0,0 +1,21 @@
+import os
+import dotenv
+import requests
+
+def get_api_key() -> str:
+    dotenv.load_dotenv()
+    API_KEY: str = os.environ.get("WEATHER_API_KEY")
+    return API_KEY
+
+def get_weather_data(city) -> dict:
+    result = requests.get(f'http://api.weatherapi.com/v1/current.json?key={get_api_key()}&q={city}')
+    json = {
+        "location": result.json()["location"]["name"],
+        "temperature": result.json()["current"]["temp_f"],
+        "wind_speed": result.json()["current"]["wind_mph"],
+        "weather": result.json()["current"]["condition"]["text"],
+        "time": result.json()["location"]["localtime"][11:],
+    }
+    return json
+
+print(get_weather_data("San Luis Obispo"))
