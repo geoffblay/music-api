d4cdd64b955c87416d5356fc6545e688bd13e9f9
crobin27 <crobin27@calpoly.edu>
2023-05-22 19:50:41 -0700

    artists tests working and complete

--- testing/test_artists.py
+++ testing/test_artists.py
@@ -8,14 +8,21 @@ client = TestClient(app)
 
 
 def test_get_artist():
-    response = client.get("/artists/2")
+    response = client.get("/artists/101")
     assert response.status_code == 200
 
-    with open("testing/artists/get_artist2.json", encoding="utf-8") as f:
+    with open("testing/artists/get_artist101.json", encoding="utf-8") as f:
         assert response.json() == json.load(f)
 
+
 def test_get_artist_404():
-    response = client.get("/artists/999999999")
+    response = client.get("/artists/-5")
     assert response.status_code == 404
 
-    assert response.json() == {"detail": "Artist not found."}
+    assert response.json() == {"detail": "Artist not found"}
+
+
+def test_get_artist2_404():
+    # unprocessable entity
+    response = client.get("/artists/af")
+    assert response.status_code == 422
