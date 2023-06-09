e2ea4acda5a62747c2cb96dfe9f2262020ab88f6
Geoff Blaylock <gblaylock2024@gmail.com>
2023-06-08 18:33:51 -0700

    added coles db.py

--- src/database.py
+++ src/database.py
@@ -30,7 +30,7 @@ engine = sqlalchemy.create_engine(database_url)
 metadata_obj = sqlalchemy.MetaData()
 
 # *********************************************************************************
-
+# autoload with None to induce a lazy load of the table metadata
 tracks = sqlalchemy.Table("tracks", metadata_obj, autoload_with=None)
 playlists = sqlalchemy.Table("playlists", metadata_obj, autoload_with=None)
 albums = sqlalchemy.Table("albums", metadata_obj, autoload_with=None)
@@ -40,10 +40,3 @@ playlist_track = sqlalchemy.Table("playlist_track", metadata_obj, autoload_with=
 album_artist = sqlalchemy.Table("album_artist", metadata_obj, autoload_with=None)
 weather = sqlalchemy.Table("weather", metadata_obj, autoload_with=None)
 users = sqlalchemy.Table("users", metadata_obj, autoload_with=None)
-
-
-def try_parse(type, val):
-    try:
-        return type(val)
-    except ValueError:
-        return None
